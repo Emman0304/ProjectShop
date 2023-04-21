027a1167a07aa839db64193888a892e6be761756
Emman0304 <emmanuel.official0304@gmail.com>
2023-04-21 17:22:15 +0800

    added user accounts tab link

--- resources/views/Admin/adminLayouts.blade.php
+++ resources/views/Admin/adminLayouts.blade.php
@@ -231,6 +231,14 @@
               </li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              {{-- <i class="nav-icon fas fa-th"></i> --}}
+              <p>
+                User Accounts
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
